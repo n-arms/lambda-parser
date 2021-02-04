e5f8c6223f2c2bbdcc8a4448482815eba1953b51
narms <75282394+n-arms@users.noreply.github.com>
2021-02-04 09:58:46 -0500

    Add build-script.sh
    * will be used to compile the interpreter

---
+++ build-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path="com/github/narms/lambda/"
+
+function usage(){
+    echo "usage: ./build_script.sh [-b]"
+}
+
+function build_files(){
+    echo "not implemented"
+}
+
+function run_frontend(){
+    echo "not implemented"
+}
