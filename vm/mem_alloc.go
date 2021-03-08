068a92c5727add68f51df807c31b25e578ae6ed2
narms <75282394+n-arms@users.noreply.github.com>
2021-03-08 17:59:52 -0500

    Add types block, runtime and some basic methods for block

--- vm/mem_alloc.go
+++ vm/mem_alloc.go
@@ -1,1 +1,37 @@
 package main
+
+const (
+  nullBlock = byte(iota)
+  numberBlock = byte(iota)
+  argumentBlock = byte(iota)
+  lambdaBlock = byte(iota)
+  applicationBlock = byte(iota)
+  pointerBlock = byte(iota)
+  listBlock = byte(iota)
+)
+
+func kindString(kind byte) string {
+  switch kind {
+  case nullBlock:
+    return "NULL"
+  case numberBlock:
+    return "number"
+  case argumentBlock:
+    return "argument"
+  case lambdaBlock:
+    return "lambda abstraction"
+  case applicationBlock:
+    return "application"
+  case pointerBlock:
+    return "pointer"
+  case listBlock:
+    return "list"
+  }
+  return ""
+}
+
+type block struct{
+  kind byte
+  left uint32
+  right uint32
+}
--- vm/runtime.go
+++ vm/runtime.go
@@ -4,6 +4,33 @@ import (
   "fmt"
 )
 
+type runtime struct {
+  heap []block
+  usedMemory uint32
+}
+
+func (r *runtime) getKind(root uint32) byte {
+  return r.heap[root].kind
+}
+
+func (r *runtime) getLeft(root uint32) uint32 {
+  return r.heap[root].left
+}
+
+func (r *runtime) getRight(root uint32) uint32 {
+  return r.heap[root].right
+}
+
+func (r *runtime) set(root uint32, kind byte, left uint32, right uint32) uint32 {
+  r.heap[root] = block{kind: kind, left: left, right: right}
+  return root
+}
+
+
 func main() {
-  fmt.Println("hello, world")
+  r := runtime{heap: make([]block, 10), usedMemory: 0}
+  r.set(0, argumentBlock, 97, 0)
+  r.set(1, argumentBlock, 98, 0)
+  r.set(2, applicationBlock, 0, 1)
+  fmt.Println(kindString(r.getKind(2)))
 }
